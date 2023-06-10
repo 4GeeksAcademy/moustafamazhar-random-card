93c5acfabe5c8d2714463d5d189bcec5d8272c97
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-10 15:11:23 +0000

    added function for cards values

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ import "./style.css";
 
 window.onload = function() {
   const suites = ["♥", "♠", "♣", "♦"];
+  const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   function randomCard(arr) {
     return Math.floor(Math.random() * arr.length);
   }
   let suite = suites[randomCard(suites)];
-  console.log(suite);
+
+  let values = cardValues[randomCard(cardValues)];
+
+  let middlesuite = document.querySelector(".icon-middle");
+  middlesuite.innerHTML = values;
 
   let topsuite = document.querySelector(".icon-top");
   topsuite.innerHTML = suite;
